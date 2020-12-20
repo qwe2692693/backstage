000650fa3a0eb342c5cd209b0913fd6b71fba3d2
77271752 <qwe1354602>
2020-12-20 10:09:51 +0800

    新增登录页面和main页面

---
+++ src/views/logn.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    登录页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: "logn"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    全部页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: "main"
+}
+</script>
+
+<style scoped>
+
+</style>
