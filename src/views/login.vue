e9d700054304a0fa5ff829966fb77626cabca15e
77271752 <qwe1354602>
2020-12-20 16:12:40 +0800

    登录页面

--- src/views/login.vue
+++ src/views/login.vue
@@ -6,14 +6,51 @@
           登录
         </span>
       </div>
-      <div class="login_content"></div>
+      <div class="login_content">
+        <el-form :model="form" label-width="70px" :rules="rules" ref="ruleForm">
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="form.username" type="text" placeholder="请输入用户名"/>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input v-model="form.password" type="password" placeholder="请输入密码"/>
+          </el-form-item>
+          <el-form-item class="t-a-r">
+            <el-button type="primary" @click="login('ruleForm')">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "login"
+  name: "login",
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      },
+      rules: {
+        username: [
+          {required: true, message: '用户名不能为空', trigger: 'blur'},
+        ],
+        password: [
+          {required: true, message: '用户名不能为空', trigger: 'blur'},
+        ]
+      }
+    }
+  },
+  methods: {
+    login(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid){
+          console.log('submit')
+        }
+      })
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
@@ -24,19 +61,22 @@ export default {
 }
 
 .login_box {
-  width: 500px;
+  width: 350px;
   background: #fff;
   border-radius: 5px;
   box-shadow: 0px 0px 46px #a8a3f5;
   margin: auto;
 }
-.login_title{
+
+.login_title {
   padding: 0 15px;
   line-height: 40px;
-  span{
+
+  span {
     position: relative;
     display: inline-block;
-    &:after{
+
+    &:after {
       position: absolute;
       bottom: 0;
       left: 0;
@@ -46,5 +86,9 @@ export default {
       background: #a8a3f5;
     }
   }
+}
+
+.login_content {
+  padding: 0 15px;
 }
 </style>
